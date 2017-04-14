5d519404e19a3e7dd08cec5181c2b78f9db40b6c
Calixto Paco <calixtopv10@gmail.com>
2017-04-13 22:53:50 -0400

    Buscando un elemento especifico en un array

--- js/array.js
+++ js/array.js
@@ -1,4 +1,18 @@
-Array.prototype.borrar = function(element){
+//1.crear una extencion remove para el objeto array
+//2.crear una extencion buscar para el objeto array
+
+//3.eliminar todos los elementos repetidos de un array
+
+//eliminando elementos repetidos
+//una forma
+var cadena1 = [ 'mario','luis','paco','mario','paco','paco'];
+Array.prototype.unique=function(a){
+  return function(){return this.filter(a)}}(function(a,b,c){return c.indexOf(a,b+1)<0
+});
+console.log( cadena1.unique() );
+
+//otra forma
+/*Array.prototype.borrar = function(element){
 	for (var i = 0; i < this.length; i++) {
 		if(this[i] === element )
 		{
@@ -8,8 +22,8 @@ Array.prototype.borrar = function(element){
 }
 
 jQuery(document).ready(function($) {
-	var datos =["gato","perro","gallo","gallo","asno","asno","perro","gato"];;
+	var datos =["gato","perro","gallo","gallo","gato","asno","asno","gallo","perro","gato"];;
 	datos.borrar("gallo");
 	//console.log(datos);
-	document.write(datos);
-});
+	console.log(datos);
+});*/
